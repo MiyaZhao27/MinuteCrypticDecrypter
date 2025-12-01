f987cfabc4f8c2861675294ae97f474af63caf88
MiyaZhao27 <miya.zhao@yale.edu>
2025-12-01 00:14:02 -0500

    getrid of word2vec

--- word2vec.py
+++ word2vec.py
@@ -1,13 +1,14 @@
 from gensim.models import KeyedVectors
 
-model = None
+MODEL_PATH = "glove.6B.50d.txt"
 
 
 def get_model():
-    global model
-    if model is None:
-        print("Loading fast GloVe 50d (Word2Vec format)...")
-        model = KeyedVectors.load_word2vec_format(
-            "glove50_word2vec.txt", binary=False)
-        print("Loaded.")
+    print("Loading GloVe embeddings (50d)...")
+    model = KeyedVectors.load_word2vec_format(
+        MODEL_PATH,
+        binary=False,
+        no_header=True
+    )
+    print("Done!")
     return model
